9c2bf0c594549787499d3917f4c6fb613f4a3363
gustavocoleta <gustavo.coleta@vrsoft.com.br>
2024-03-18 22:14:09 -0300

    add postgres connection test

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 // console.log("Hello, CodeLab!!!")
 
 import express from 'express'
-const app = express()
-const port = 3000
+import { Client } from 'pg'
 
-app.get('/', (req, res) => {
-  res.send('Hello, CodeLab...')
-})
+async function main() {
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  const app = express()
+  const port = 3000
+
+  const client = new Client()
+  await client.connect()
+  
+  const res = await client.query('SELECT $1::text as message', ['DB Connection is OK!'])
+  console.log(res.rows[0].message)
+  await client.end()
+
+  app.get('/', (req, res) => {
+    res.send('Hello, CodeLab...')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+}
+
+main()
